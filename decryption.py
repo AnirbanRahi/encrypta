d131ccf3f038120fc2ae9536db310265ffec4cc5
AnirbanRahi <anirbanrahi.9090@gmail.com>
2026-01-21 20:19:38 +0600

    Convert encryption.py and decryption.py to OOP style

--- decryption.py
+++ decryption.py
@@ -8,74 +8,76 @@ from cryptography.exceptions import InvalidTag, InvalidSignature
 from cryptography.hazmat.primitives import hmac, hashes
 
 
-def passwordkey(password, salt):
-    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=200000, backend=default_backend())
-    return kdf.derive(password)
+class Decryptor:
+    def __init__(self, readsize=512 * 1024, iteration=200000):
+        self.readsize = readsize
+        self.iterations = iteration
+        self.backend = default_backend()
 
+    def _passwordkey(self, password, salt):
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=self.iterations,
+                         backend=self.backend)
+        return kdf.derive(password)
 
-def fixpath(filepath):
-    if filepath[0] == "\"" or filepath[0] == "\'":
-        filepath = filepath[1:]
-    if filepath[-1] == "\"" or filepath[-1] == "\'":
-        filepath = filepath[:-1]
-    filepath = filepath.replace("\\", "/")
-    filepath = filepath.replace("\\\\", "/")
-    return filepath
+    def _fixpath(self, filepath):
+        if not filepath:
+            raise ValueError("Empty file path")
+        if filepath[0] == "\"" or filepath[0] == "\'":
+            filepath = filepath[1:]
+        if filepath[-1] == "\"" or filepath[-1] == "\'":
+            filepath = filepath[:-1]
+        filepath = filepath.replace("\\", "/")
+        filepath = filepath.replace("\\\\", "/")
+        return filepath
 
+    def decrypt(self, file: str, password: str):
 
-file = input("Enter File Path: ").strip()
-file = fixpath(file)
+        file = self._fixpath(file)
+        p = Path(file)
+        filepath = p.parent
+        filename = p.stem
+        if p.suffix != '.enc':
+            raise ValueError(f"{filename} is not encrypted. Try another file.")
 
-p = Path(file)
-filepath = p.parent
-filename = p.stem
-fileextension = p.suffix
-if p.suffix != '.enc':
-    print(f"{filename} is not encrypted. Try another file.")
-    exit(1)
-newfile = filepath / filename
+        newfile = filepath / filename
+        password = password.encode('utf-8')
 
-password = input("Password: ").encode('utf-8')
+        with open(file, "rb") as finput, open(newfile, "wb") as foutput:
+            salt = finput.read(16)
+            nonce = finput.read(12)
+            passkey = finput.read(32)
+            key = self._passwordkey(password, salt)
+            h = hmac.HMAC(key, hashes.SHA256())
+            h.update(salt)
+            try:
+                h.verify(passkey)
+            except InvalidSignature:
+                foutput.close()
+                os.remove(newfile)
+                raise ValueError("Wrong password")
+            filesize = os.path.getsize(file)
+            encryptedsize = filesize - 16 - 12 - 16 - 32  # total ciphertext size
+            tagposition = filesize - 16
+            finput.seek(tagposition)
+            tag = finput.read(16)
+            cipher = Cipher(algorithms.AES(key), modes.GCM(nonce, tag), backend=self.backend)
+            decryptor = cipher.decryptor()
 
-readsize = 512 * 1024  # 512 KB
+            finput.seek(16 + 12 + 32)  # skip salt + nonce
+            remaining = encryptedsize
+            while remaining > 0:
+                data = finput.read(min(self.readsize, remaining))
+                if not data:
+                    break
+                originaldata = decryptor.update(data)
+                foutput.write(originaldata)
+                remaining = remaining - len(data)
+            try:
+                decryptor.finalize()
+            except InvalidTag:
+                foutput.close()
+                os.remove(newfile)
+                raise ValueError("Decryption failed: wrong password or corrupted file")
 
-with open(file, "rb") as finput, open(newfile, "wb") as foutput:
-    salt = finput.read(16)
-    nonce = finput.read(12)
-    passkey = finput.read(32)
-    key = passwordkey(password, salt)
-    h = hmac.HMAC(key, hashes.SHA256())
-    h.update(salt)
-    try:
-        h.verify(passkey)
-    except InvalidSignature:
-        print("Wrong password")
-        foutput.close()
-        os.remove(newfile)
-        exit(1)
-    filesize = os.path.getsize(file)
-    encryptedsize = filesize - 16 - 12 - 16 - 32  # total ciphertext size
-    tagposition = filesize - 16
-    finput.seek(tagposition)
-    tag = finput.read(16)
-    cipher = Cipher(algorithms.AES(key), modes.GCM(nonce, tag), backend=default_backend())
-    decryptor = cipher.decryptor()
-
-    finput.seek(16 + 12 + 32)  # skip salt + nonce
-    remaining = encryptedsize
-    while remaining > 0:
-        data = finput.read(min(readsize, remaining))
-        if not data:
-            break
-        originaldata = decryptor.update(data)
-        foutput.write(originaldata)
-        remaining = remaining - len(data)
-    try:
-        decryptor.finalize()
-    except InvalidTag:
-        print("Decryption failed: wrong password or corrupted file")
-        foutput.close()
-        os.remove(newfile)
-        exit(1)
-
-print("Successfully decrypted")
+        print("Successfully decrypted")
+        return newfile
--- encryption.py
+++ encryption.py
@@ -8,60 +8,65 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hmac, hashes
 
 
-def passwordkey(password, salt):
-    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=200000, backend=default_backend())
-    return kdf.derive(password)
+class Encryptor:
+    def __init__(self, readsize=512 * 1024, iteration=200000):
+        self.readsize = readsize
+        self.iterations = iteration
+        self.backend = default_backend()
 
+    def _passwordkey(self, password, salt):
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=self.iterations,
+                         backend=self.backend)
+        return kdf.derive(password)
 
-def fixpath(filepath):
-    if filepath[0] == "\"" or filepath[0] == "\'":
-        filepath = filepath[1:]
-    if filepath[-1] == "\"" or filepath[-1] == "\'":
-        filepath = filepath[:-1]
-    filepath = filepath.replace("\\", "/")
-    filepath = filepath.replace("\\\\", "/")
-    return filepath
+    def _fixpath(self, filepath):
+        if not filepath:
+            raise ValueError("Empty file path")
+        if filepath[0] == "\"" or filepath[0] == "\'":
+            filepath = filepath[1:]
+        if filepath[-1] == "\"" or filepath[-1] == "\'":
+            filepath = filepath[:-1]
+        filepath = filepath.replace("\\", "/")
+        filepath = filepath.replace("\\\\", "/")
+        return filepath
 
+    def encrypt(self, file: str, password: str):
 
-file = input("Enter File Path: ").strip()
-file = fixpath(file)
+        file = self._fixpath(file)
+        p = Path(file)
+        filepath = p.parent
+        filename = p.stem
+        fileextension = p.suffix
+        if p.suffix == '.enc':
+            raise ValueError("File is already encrypted")
 
-p = Path(file)
-filepath = p.parent
-filename = p.stem
-fileextension = p.suffix
-if p.suffix == '.enc':
-    print(f"{filename} is already encrypted. Try another file.")
-    exit(1)
-newfile = filepath / (filename + fileextension + ".enc")
+        newfile = filepath / (filename + fileextension + ".enc")
 
-password = input("Password: ").encode('utf-8')
+        password = password.encode('utf-8')
 
-readsize = 512 * 1024  # 512 KB
-nonce = os.urandom(12)
-salt = os.urandom(16)
+        nonce = os.urandom(12)
+        salt = os.urandom(16)
 
-key = passwordkey(password, salt)
-cipher = Cipher(algorithms.AES(key), modes.GCM(nonce), backend=default_backend())
-encryptor = cipher.encryptor()
+        key = self._passwordkey(password, salt)
+        cipher = Cipher(algorithms.AES(key), modes.GCM(nonce), backend=self.backend)
+        encryptor = cipher.encryptor()
 
-h = hmac.HMAC(key, hashes.SHA256())
-h.update(salt)
-passkey = h.finalize()
+        h = hmac.HMAC(key, hashes.SHA256())
+        h.update(salt)
+        passkey = h.finalize()
 
-with open(file, "rb") as finput, open(newfile, "wb") as foutput:
-    foutput.write(salt)
-    foutput.write(nonce)
-    foutput.write(passkey)
-    while True:
-        data = finput.read(readsize)  # try to read upto readsize and return it
-        if not data:
-            break
-        ciphertext = encryptor.update(data)
-        foutput.write(ciphertext)
+        with open(file, "rb") as finput, open(newfile, "wb") as foutput:
+            foutput.write(salt)
+            foutput.write(nonce)
+            foutput.write(passkey)
+            while True:
+                data = finput.read(self.readsize)  # try to read upto readsize and return it
+                if not data:
+                    break
+                ciphertext = encryptor.update(data)
+                foutput.write(ciphertext)
 
-    foutput.write(encryptor.finalize())
-    foutput.write(encryptor.tag)
+            foutput.write(encryptor.finalize())
+            foutput.write(encryptor.tag)
 
-print("Original file size:", os.path.getsize(file))
-print("Encrypted file size:", os.path.getsize(newfile))
+        return newfile
